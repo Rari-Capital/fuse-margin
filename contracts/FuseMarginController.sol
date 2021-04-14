9629e93b1337e825e36067879ea5afda40bdcc88
Gautham Ganesh Elango <gautham.gg@gmail.com>
2021-04-14 23:27:13 +1000

    renamed events

--- contracts/FuseMarginController.sol
+++ contracts/FuseMarginController.sol
@@ -27,7 +27,7 @@ contract FuseMarginController is IFuseMarginController, ERC721, Ownable {
 
     /// @dev Ensures functions are called from approved FuseMargin contracts
     modifier onlyMargin() {
-        require(approvedContracts[msg.sender], "not approved contract");
+        require(approvedContracts[msg.sender], "FuseMarginController: Not approved contract");
         _;
     }
 
@@ -51,7 +51,7 @@ contract FuseMarginController is IFuseMarginController, ERC721, Ownable {
     /// @dev Adds support for a new FuseMargin contract, to be called only from owner
     /// @param contractAddress Address of FuseMargin contract
     function addMarginContract(address contractAddress) external override onlyOwner {
-        require(!approvedContracts[contractAddress], "already exists");
+        require(!approvedContracts[contractAddress], "FuseMarginController: Already exists");
         marginContracts.push(contractAddress);
         approvedContracts[contractAddress] = true;
         marginContractsNum++;
@@ -61,7 +61,7 @@ contract FuseMarginController is IFuseMarginController, ERC721, Ownable {
     /// @dev Removes support for a new FuseMargin contract, to be called only from owner
     /// @param contractAddress Address of FuseMargin contract
     function removeMarginContract(address contractAddress) external override onlyOwner {
-        require(approvedContracts[contractAddress], "does not exist");
+        require(approvedContracts[contractAddress], "FuseMarginController: Does not exist");
         approvedContracts[contractAddress] = false;
         marginContractsNum--;
         emit RemoveMarginContract(contractAddress, msg.sender);
