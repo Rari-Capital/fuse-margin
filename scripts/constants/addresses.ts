8cbf87255a8605e2b90f5baef36af29530c4cbd3
Gautham Ganesh Elango <gautham.gg@gmail.com>
2021-04-15 09:18:16 +1000

    added addresses

--- scripts/constants/addresses.ts
+++ scripts/constants/addresses.ts
@@ -1,3 +1,8 @@
 export const soloMarginAddress: string = "0x1E0447b19BB6EcFdAe1e4AE1694b0C3659614e4e";
 export const lendingPoolProviderAddress: string = "0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5";
 export const uniswapFactoryAddress: string = "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f";
+export const fusePool4: string = "0x6E7fb6c5865e8533D5ED31b6d43fD95f4C411834";
+export const fr4WBTC: string = "0x8ad7472631695420626C17EE0F8CEcfEA480cAFf";
+export const fr4USDC: string = "0x94C49563a3950424a2a7790c3eF5458A2A359C7e";
+export const fr4DAI: string = "0xCF90552eC3Db6143C83385FdD7bC96ef40F7ee44";
+export const fr4RGT: string = "0x0D90341220fb052a3C5E79D8fb1C6f26f9c9Fdc9";
