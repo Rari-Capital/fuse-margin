79e57bd75f7ec15d1b29c4b4415dcfc7879e133a
Gautham Ganesh Elango <gautham.gg@gmail.com>
2021-04-15 10:29:56 +1000

    position

---
+++ test/Position.ts
@@ -0,0 +1,88 @@
+import { ethers } from "hardhat";
+import { Signer, Wallet, BigNumber } from "ethers";
+import { expect } from "chai";
+import {
+  FuseMarginController,
+  FuseMarginV1,
+  Position,
+  FuseMarginController__factory,
+  FuseMarginV1__factory,
+  Position__factory,
+} from "../typechain";
+import { fuseMarginControllerName, fuseMarginControllerSymbol } from "../scripts/constants/constructors";
+import { soloMarginAddress, uniswapFactoryAddress } from "../scripts/constants/addresses";
+
+describe("Position", () => {
+  let accounts: Signer[];
+  let owner: Wallet;
+  let attacker: Wallet;
+  let fuseMarginController: FuseMarginController;
+  let position: Position;
+  let fuseMarginV1: FuseMarginV1;
+
+  beforeEach(async () => {
+    accounts = await ethers.getSigners();
+    owner = <Wallet>accounts[0];
+    attacker = <Wallet>accounts[1];
+
+    const fuseMarginControllerFactory: FuseMarginController__factory = (await ethers.getContractFactory(
+      "contracts/FuseMarginController.sol:FuseMarginController",
+      owner,
+    )) as FuseMarginController__factory;
+    fuseMarginController = await fuseMarginControllerFactory.deploy(
+      fuseMarginControllerName,
+      fuseMarginControllerSymbol,
+    );
+
+    const positionFactory: Position__factory = (await ethers.getContractFactory(
+      "contracts/Position.sol:Position",
+      owner,
+    )) as Position__factory;
+    position = await positionFactory.deploy();
+    await position.initialize(fuseMarginController.address);
+    const fuseMarginV1Factory: FuseMarginV1__factory = (await ethers.getContractFactory(
+      "contracts/FuseMarginV1.sol:FuseMarginV1",
+      owner,
+    )) as FuseMarginV1__factory;
+    fuseMarginV1 = await fuseMarginV1Factory.deploy(
+      fuseMarginController.address,
+      position.address,
+      uniswapFactoryAddress,
+      soloMarginAddress,
+    );
+    await fuseMarginController.addMarginContract(fuseMarginV1.address);
+  });
+
+  it("constructor should initialize state variables", async () => {
+    const getName: string = await fuseMarginController.name();
+    expect(getName).to.equal(fuseMarginControllerName);
+    const getSymbol: string = await fuseMarginController.symbol();
+    expect(getSymbol).to.equal(fuseMarginControllerSymbol);
+    const getOwner: string = await fuseMarginController.owner();
+    expect(getOwner).to.equal(owner.address);
+    const getMarginContracts: string = await fuseMarginController.marginContracts(BigNumber.from(0));
+    expect(getMarginContracts).to.equal(fuseMarginV1.address);
+    const getGetMarginContracts: string[] = await fuseMarginController.getMarginContracts();
+    expect(getGetMarginContracts).to.deep.equal([fuseMarginV1.address]);
+    const getApprovedContracts: boolean = await fuseMarginController.approvedContracts(fuseMarginV1.address);
+    expect(getApprovedContracts).to.equal(true);
+    const [getTokensOfOwner, getPositionsOfOwner]: [BigNumber[], string[]] = await fuseMarginController.tokensOfOwner(
+      owner.address,
+    );
+    expect(getTokensOfOwner).to.deep.equal([]);
+    expect(getPositionsOfOwner).to.deep.equal([]);
+
+    const fuseMarginController0: string = await position.fuseMarginController();
+    expect(fuseMarginController0).to.equal(fuseMarginController.address);
+    const getUniswapFactory: string = await fuseMarginV1.uniswapFactory();
+    expect(getUniswapFactory).to.equal(uniswapFactoryAddress);
+    const getSoloMargin: string = await fuseMarginV1.soloMargin();
+    expect(getSoloMargin).to.equal(soloMarginAddress);
+    const getFuseMarginController1: string = await fuseMarginV1.fuseMarginController();
+    expect(getFuseMarginController1).to.equal(fuseMarginController.address);
+    const getFuseMarginERC721: string = await fuseMarginV1.fuseMarginERC721();
+    expect(getFuseMarginERC721).to.equal(fuseMarginController.address);
+    const getPositionImplementation: string = await fuseMarginV1.positionImplementation();
+    expect(getPositionImplementation).to.equal(position.address);
+  });
+});
