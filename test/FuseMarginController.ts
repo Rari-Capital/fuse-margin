2d0124c9696068dd8370e01fe9bef88cb7b6a817
Gautham Ganesh Elango <gautham.gg@gmail.com>
2021-04-15 10:16:10 +1000

    controller security checks

--- test/FuseMarginController.ts
+++ test/FuseMarginController.ts
@@ -17,6 +17,7 @@ describe("FuseMarginController", () => {
   let owner: Wallet;
   let attacker: Wallet;
   let fuseMarginController: FuseMarginController;
+  let fuseMarginV1Factory: FuseMarginV1__factory;
   let position: Position;
   let fuseMarginV1: FuseMarginV1;
 
@@ -40,7 +41,7 @@ describe("FuseMarginController", () => {
     )) as Position__factory;
     position = await positionFactory.deploy();
     await position.initialize(fuseMarginController.address);
-    const fuseMarginV1Factory: FuseMarginV1__factory = (await ethers.getContractFactory(
+    fuseMarginV1Factory = (await ethers.getContractFactory(
       "contracts/FuseMarginV1.sol:FuseMarginV1",
       owner,
     )) as FuseMarginV1__factory;
@@ -99,7 +100,7 @@ describe("FuseMarginController", () => {
     );
   });
 
-  it("should add and remove margin contracts", async () => {
+  it("should add margin contracts", async () => {
     await expect(fuseMarginController.marginContracts(BigNumber.from(0))).to.be.reverted;
     const getApprovedContract0: boolean = await fuseMarginController.approvedContracts(fuseMarginV1.address);
     expect(getApprovedContract0).to.equal(false);
@@ -110,16 +111,64 @@ describe("FuseMarginController", () => {
     expect(getMarginContracts1).to.equal(fuseMarginV1.address);
     const getApprovedContract1: boolean = await fuseMarginController.approvedContracts(fuseMarginV1.address);
     expect(getApprovedContract1).to.equal(true);
+    const getGetMarginContracts1: string[] = await fuseMarginController.getMarginContracts();
+    expect(getGetMarginContracts1).to.deep.equal([fuseMarginV1.address]);
+    await expect(fuseMarginController.addMarginContract(fuseMarginV1.address)).to.be.revertedWith(
+      "FuseMarginController: Already exists",
+    );
 
-    const fuseMarginV1Factory: FuseMarginV1__factory = (await ethers.getContractFactory(
-      "contracts/FuseMarginV1.sol:FuseMarginV1",
-      owner,
-    )) as FuseMarginV1__factory;
     const fuseMarginV10: FuseMarginV1 = await fuseMarginV1Factory.deploy(
       fuseMarginController.address,
       position.address,
       uniswapFactoryAddress,
       soloMarginAddress,
+    );
+    await expect(fuseMarginController.marginContracts(BigNumber.from(1))).to.be.reverted;
+    const getApprovedContract2: boolean = await fuseMarginController.approvedContracts(fuseMarginV10.address);
+    expect(getApprovedContract2).to.equal(false);
+    await expect(fuseMarginController.addMarginContract(fuseMarginV10.address))
+      .to.emit(fuseMarginController, "AddMarginContract")
+      .withArgs(fuseMarginV10.address, owner.address);
+    const getMarginContracts3: string = await fuseMarginController.marginContracts(BigNumber.from(1));
+    expect(getMarginContracts3).to.equal(fuseMarginV10.address);
+    const getApprovedContract3: boolean = await fuseMarginController.approvedContracts(fuseMarginV10.address);
+    expect(getApprovedContract3).to.equal(true);
+    const getGetMarginContracts3: string[] = await fuseMarginController.getMarginContracts();
+    expect(getGetMarginContracts3).to.deep.equal([fuseMarginV1.address, fuseMarginV10.address]);
+    await expect(fuseMarginController.addMarginContract(fuseMarginV10.address)).to.be.revertedWith(
+      "FuseMarginController: Already exists",
+    );
+  });
+
+  it("should remove margin contracts", async () => {
+    await fuseMarginController.addMarginContract(fuseMarginV1.address);
+    const fuseMarginV10: FuseMarginV1 = await fuseMarginV1Factory.deploy(
+      fuseMarginController.address,
+      position.address,
+      uniswapFactoryAddress,
+      soloMarginAddress,
+    );
+    await expect(fuseMarginController.removeMarginContract(fuseMarginV10.address)).to.be.revertedWith(
+      "FuseMarginController: Does not exist",
+    );
+    await fuseMarginController.addMarginContract(fuseMarginV10.address);
+    const getMarginContracts0: string = await fuseMarginController.marginContracts(BigNumber.from(1));
+    expect(getMarginContracts0).to.equal(fuseMarginV10.address);
+    const getApprovedContract0: boolean = await fuseMarginController.approvedContracts(fuseMarginV10.address);
+    expect(getApprovedContract0).to.equal(true);
+    const getGetMarginContracts0: string[] = await fuseMarginController.getMarginContracts();
+    expect(getGetMarginContracts0).to.deep.equal([fuseMarginV1.address, fuseMarginV10.address]);
+    await expect(fuseMarginController.removeMarginContract(fuseMarginV10.address))
+      .to.emit(fuseMarginController, "RemoveMarginContract")
+      .withArgs(fuseMarginV10.address, owner.address);
+    const getMarginContracts1: string = await fuseMarginController.marginContracts(BigNumber.from(1));
+    expect(getMarginContracts1).to.equal(fuseMarginV10.address);
+    const getApprovedContract1: boolean = await fuseMarginController.approvedContracts(fuseMarginV10.address);
+    expect(getApprovedContract1).to.equal(false);
+    const getGetMarginContracts1: string[] = await fuseMarginController.getMarginContracts();
+    expect(getGetMarginContracts1).to.deep.equal([fuseMarginV1.address]);
+    await expect(fuseMarginController.removeMarginContract(fuseMarginV10.address)).to.be.revertedWith(
+      "FuseMarginController: Does not exist",
     );
   });
 });
