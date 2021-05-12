ceb9dd26108af11364a716a00dbf51b254c9f359
Gautham Ganesh Elango <gautham.gg@gmail.com>
2021-05-12 20:18:50 +1000

    fix

--- test/FuseMarginV1.ts
+++ test/FuseMarginV1.ts
@@ -286,7 +286,7 @@ describe("FuseMarginV1", () => {
       .withArgs(ethers.constants.AddressZero, impersonateAddress, BigNumber.from(0));
     const getPositions1 = await fuseMarginController.positions(BigNumber.from(0));
     const getfr4WBTCBalance1 = await fr4WBTC.balanceOfUnderlying(getPositions1);
-
+    console.log(getfr4WBTCBalance1.toString());
     const wbtcAddAmount: BigNumber = BigNumber.from("10000000");
     await WBTC.connect(impersonateAddressSigner).approve(fuseMarginV1.address, wbtcAddAmount);
     await expect(
@@ -295,6 +295,7 @@ describe("FuseMarginV1", () => {
         .addToPosition(WBTC.address, fr4WBTC.address, fusePool4, [], false, BigNumber.from(0), wbtcAddAmount),
     );
     const getfr4WBTCBalance2 = await fr4WBTC.balanceOfUnderlying(getPositions1);
+    console.log(getfr4WBTCBalance2.toString());
     expect(getfr4WBTCBalance2).to.be.gte(getfr4WBTCBalance1.add(wbtcAddAmount));
   });
 
@@ -334,7 +335,7 @@ describe("FuseMarginV1", () => {
       .withArgs(ethers.constants.AddressZero, impersonateAddress, BigNumber.from(0));
     const getPositions1 = await fuseMarginController.positions(BigNumber.from(0));
     const getfr4WBTCBalance1 = await fr4WBTC.balanceOfUnderlying(getPositions1);
-
+    console.log(getfr4WBTCBalance1.toString());
     const wbtcAddAmount: BigNumber = BigNumber.from("10000000");
     await WBTC.connect(impersonateAddressSigner).approve(fuseMarginV1.address, wbtcAddAmount);
     await expect(
@@ -351,6 +352,7 @@ describe("FuseMarginV1", () => {
         ),
     );
     const getfr4WBTCBalance2 = await fr4WBTC.balanceOfUnderlying(getPositions1);
+    console.log(getfr4WBTCBalance2.toString());
     expect(getfr4WBTCBalance2).to.be.gte(getfr4WBTCBalance1.add(wbtcAddAmount));
   });
 
